34e5501d92e0f8401c857bd0e2e07a6dca0932e6
Aurelien Marc <aurelienmarc2012@gmail.com>
2020-12-06 09:46:24 +0100

    P2MG1-69
    Rajout d'un tableau de bord, d'une page d'accueil ainsi qu'un menu. création de tout les compenent principal

---
+++ src/app/pages/gestion-abs/app-gestion-abs/app-gestion-abs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-gestion-abs',
+  templateUrl: './app-gestion-abs.component.html',
+  styleUrls: ['./app-gestion-abs.component.css']
+})
+export class AppGestionAbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  change(s: string) {
+    this.router.navigate([s]);
+  }
+
+  // tslint:disable-next-line:typedef
+  logged(){
+    return sessionStorage.getItem('loggedIn');
+  }
 }
